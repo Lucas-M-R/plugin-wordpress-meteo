5abf003b6903e05924171785b62173b17de7a5b1
LucasMR <lucas.mroy@gmail.com>
2021-11-26 23:02:48 +0100

    ajout presentation meteo

--- includes/Views/currentweather.php
+++ includes/Views/currentweather.php
@@ -1,13 +1,17 @@
 <?php
 
 
-$location = 'dijon';
+$location = 'moscow';
 $currentWeather = new Openweather();
 $weather = $currentWeather->getCurrentWeather($location);
 $weatherImg = 'http://openweathermap.org/img/wn/' . $weather["weatherIcon"] . '@2x.png';
 $wind = new Openweather();
 $windDirection = $wind->wind_cardinals('150');
 $accordCardinal = $wind->accordCardinal($windDirection);
+
+
+
+
 
 // resultats de la requette : 
 //      
@@ -22,12 +26,14 @@ $accordCardinal = $wind->accordCardinal($windDirection);
 //   $weather['weatherIcon']
 
 echo '<br/>';
+echo '<div style="background:grey;"><img src="'.$weatherImg.'"></div>';
+echo '<br/>';
 echo 'il fait ' . $weather['temp'] . '°C<br/>';
 echo 'il fait ' . $weather['feels_like'] . '°C en ressenti <br/>';
-echo 'il fait ' . $weather['temp_min'] . '°C minimum<br/>';
-echo 'il fait ' . $weather['temp_max'] . '°C maximum<br/>';
+echo 'il fait ' . $weather['temp_min'] . '°C au minimum<br/>';
+echo 'il fait ' . $weather['temp_max'] . '°C au maximum<br/>';
 echo 'il fait ' . $weather['humidity'] . '% d\'humidité<br/>';
 echo 'il souffle ' . $weather['wind_speed'] . 'km/h de vent<br/>';
 echo 'Un vent qui souffle  ' . $accordCardinal .' '. $windDirection . '<br/>';
 echo 'il fait ' . $weather['weatherDesc'] . '<br/>';
-echo 'à <img src="'.$weatherImg.'">';
+echo 'à '. $weather['city'];
